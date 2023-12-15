0be0f99d2576d5cebf70ea25d7ec2d3368a1fc8c
Eero <eeero93@gmail.com>
2023-12-15 20:40:44 +0200

    Make script recursive

--- filecount.sh
+++ filecount.sh
@@ -9,10 +9,10 @@ folder_path=$(pwd)
 
 if [ $# -eq 1 ]; then
 	# If a parameter is provided, count files containing the specified string
-	string_count=$(find "$folder_path" -maxdepth 1 -type f -name "*$1*" | wc -l)
-	echo "Number of files in $folder_path containing '$1': $string_count"
+	string_count=$(find "$folder_path" -type f -name "*$1*" | wc -l)
+	echo "Filenames in $folder_path with '$1': $string_count"
 else
-	file_count=$(find "$folder_path" -maxdepth 1 -type f | wc -l)
+	file_count=$(find "$folder_path" -type f | wc -l)
 	echo "Number of files in $folder_path: $file_count"
 fi
 # else
