b2d65bec2435909eb48562400464e1d31121a9a1
eerokos <eerokos@eerot490.v6.elisa-mobile.fi>
2024-01-31 13:05:06 +0200

    Add function to check if r_env is active and change shell_cmd accordingly

--- nvim/lua/plugins/jukit.lua
+++ nvim/lua/plugins/jukit.lua
@@ -1,8 +1,23 @@
-return { {
-  "luk400/vim-jukit",
-  lazy = true,
-  ft = {
-    "ipynb",
-    "jupyter",
+-- local g = vim.g
+-- local map = vim.keymap.set
+--
+-- local function check_env_var()
+--   local conda_env_name = os.getenv("CONDA_DEFAULT_ENV")
+--   if conda_env_name == "r_env" then
+--     g.jukit_shell_cmd = "R"
+--     map("n", "<leader>os", '<cmd>call jukit#splits#output("conda activate r_env")<cr>', { desc = "Open output split" })
+--   end
+-- end
+
+return {
+  {
+    "luk400/vim-jukit",
+    lazy = true,
+    -- cond = vim.env.VAR_NAME == 0,
+    ft = {
+      "ipynb",
+      "jupyter",
+    },
   },
-} }
+  -- check_env_var(),
+}
