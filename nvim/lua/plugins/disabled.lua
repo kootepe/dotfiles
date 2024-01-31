57a15d862e86b7b89ba2f113b018b9121d1b49c0
Eero <eeero93@gmail.com>
2024-01-31 23:31:49 +0200

    Disable hexokinase

--- nvim/lua/plugins/disabled.lua
+++ nvim/lua/plugins/disabled.lua
@@ -3,4 +3,5 @@ return {
   { "echasnovski/mini.indentscope", enabled = false },
   { "lukas-reineke/indent-blankline.nvim", enabled = false },
   { "m-demare/hlargs.nvim", enabled = false },
+  { "Rrethy/vim-hexokinase", enabled = false },
 }
