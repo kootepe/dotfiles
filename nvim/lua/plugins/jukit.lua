98dd4b988aa9c291573989ea5bd5fe1968e7504d
Eero <eeero93@gmail.com>
2024-01-31 14:36:42 +0200

    Add jukit function to check r_env

--- nvim/lua/plugins/jukit.lua
+++ nvim/lua/plugins/jukit.lua
@@ -1,13 +1,13 @@
--- local g = vim.g
--- local map = vim.keymap.set
---
--- local function check_env_var()
---   local conda_env_name = os.getenv("CONDA_DEFAULT_ENV")
---   if conda_env_name == "r_env" then
---     g.jukit_shell_cmd = "R"
---     map("n", "<leader>os", '<cmd>call jukit#splits#output("conda activate r_env")<cr>', { desc = "Open output split" })
---   end
--- end
+local g = vim.g
+local map = vim.keymap.set
+
+local function check_env_var()
+  local conda_env_name = os.getenv("CONDA_DEFAULT_ENV")
+  if conda_env_name == "r_env" then
+    g.jukit_shell_cmd = "R"
+    map("n", "<leader>os", '<cmd>call jukit#splits#output("conda activate r_env")<cr>', { desc = "Open output split" })
+  end
+end
 
 return {
   {
@@ -19,5 +19,5 @@ return {
       "jupyter",
     },
   },
-  -- check_env_var(),
+  check_env_var(),
 }
