c84619e40faa4e5a52fc1bdbddb728df548647e9
Eero <eeero93@gmail.com>
2024-02-02 09:39:52 +0200

    Better r shell

--- nvim/lua/plugins/jukit.lua
+++ nvim/lua/plugins/jukit.lua
@@ -4,7 +4,7 @@ local map = vim.keymap.set
 local function check_env_var()
   local conda_env_name = os.getenv("CONDA_DEFAULT_ENV")
   if conda_env_name == "r_env" then
-    g.jukit_shell_cmd = "R"
+    g.jukit_shell_cmd = "radian"
     map("n", "<leader>os", '<cmd>call jukit#splits#output("conda activate r_env")<cr>', { desc = "Open output split" })
   end
 end
