5ddd8413dd7d8ac807aad330511aef82b4fffab2
Eero <eeero93@gmail.com>
2024-02-16 18:14:45 +0200

    Add personal fork of friendly snippets

--- nvim/lua/plugins/luasnip.lua
+++ nvim/lua/plugins/luasnip.lua
@@ -1,6 +1,7 @@
 return {
   {
     "L3MON4D3/LuaSnip",
+    dependencies = { "kootepe/friendly-snippets" },
     config = function()
       require("luasnip.loaders.from_snipmate").load({ paths = "~/.config/nvim/snippets" })
     end,
