ea1b8c73f4564f6390dde75cae18eedd5367b708
Eero <eeero93@gmail.com>
2024-05-30 08:06:35 +0300

    Remove keybinds

--- nvim/lua/plugins/bufferline.lua
+++ nvim/lua/plugins/bufferline.lua
@@ -13,15 +13,9 @@ return {
       },
     },
     keys = {
-      { "<leader>bp", "<Cmd>BufferLineTogglePin<CR>", desc = "Toggle pin" },
-      { "<leader>bP", "<Cmd>BufferLineGroupClose ungrouped<CR>", desc = "Delete non-pinned buffers" },
-      { "<leader>bo", "<Cmd>BufferLineCloseOthers<CR>", desc = "Delete other buffers" },
-      { "<leader>br", "<Cmd>BufferLineCloseRight<CR>", desc = "Delete buffers to the right" },
-      { "<leader>bl", "<Cmd>BufferLineCloseLeft<CR>", desc = "Delete buffers to the left" },
-      { "<A-,>", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev buffer" },
-      { "<A-.>", "<cmd>BufferLineCycleNext<cr>", desc = "Next buffer" },
-      { "[b", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev buffer" },
-      { "]b", "<cmd>BufferLineCycleNext<cr>", desc = "Next buffer" },
+      -- { "<leader>bl", "<Cmd>BufferLineCloseLeft<CR>", desc = "Delete buffers to the left" },
+      -- { "S-ö", "<cmd>BufferLineMoveNext<CR>", desc = "Move buffer forward" },
+      -- { "S-ä", "<cmd>BufferLineMovePrev<CR>", desc = "Move buffer backward" },
     },
   },
 }
