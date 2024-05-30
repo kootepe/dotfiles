94a816e42c539a0e9b8eabe6cd2732a7c662c830
Eero <eeero93@gmail.com>
2024-05-30 08:10:42 +0300

    something

--- nvim/lua/plugins/disabled.lua
+++ nvim/lua/plugins/disabled.lua
@@ -5,3 +5,5 @@ return {
   { "m-demare/hlargs.nvim", enabled = true },
   { "Rrethy/vim-hexokinase", enabled = false },
 }
+-- this might fix nvim-cmp crashing from time to time
+-- require("nvim-autopairs").setup({check_ts = true, map_cr = false})
