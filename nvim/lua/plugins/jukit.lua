a39c48efb5f8e9386241541bde7d75cadfb58768
Eero <eeero93@gmail.com>
2024-09-14 18:41:56 +0300

    Enable conda env when using R

--- nvim/lua/plugins/jukit.lua
+++ nvim/lua/plugins/jukit.lua
@@ -6,6 +6,14 @@ local function check_env_var()
   if conda_env_name == "r_env" then
     g.jukit_shell_cmd = "radian"
     map("n", "<leader>os", '<cmd>call jukit#splits#output("conda activate r_env")<cr>', { desc = "Open output split" })
+  elseif conda_env_name == "johan_env" then
+    g.jukit_shell_cmd = "R"
+    map(
+      "n",
+      "<leader>os",
+      '<cmd>call jukit#splits#output("conda activate johan_env")<cr>',
+      { desc = "Open output split" }
+    )
   end
 end
 
