dc4cc91dd822eb8462a824fc22a5b2b7ecd580f6
Eero <eeero93@gmail.com>
2024-09-14 18:42:35 +0300

    Disable indent blankline

--- nvim/lua/plugins/disabled.lua
+++ nvim/lua/plugins/disabled.lua
@@ -1,8 +1,8 @@
 return {
   { "RRethy/vim-illuminate", enabled = false },
-  { "lukas-reineke/indent-blankline.nvim", enabled = false },
   { "echasnovski/mini.indentscope", enabled = true },
   { "shellRaining/hlchunk.nvim", enabled = false },
+  { "lukas-reineke/indent-blankline.nvim", enabled = true },
   { "m-demare/hlargs.nvim", enabled = true },
   { "Rrethy/vim-hexokinase", enabled = false },
 }
