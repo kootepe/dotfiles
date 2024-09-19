ece0aa30dec5123655ed97b2e47748577c4e2ce8
Eero <eeero93@gmail.com>
2024-09-19 08:36:27 +0300

    Neogen config

--- nvim/lua/plugins/neogen.lua
+++ nvim/lua/plugins/neogen.lua
@@ -2,6 +2,15 @@ return {
   "danymat/neogen",
   dependencies = "nvim-treesitter/nvim-treesitter",
   config = true,
+  -- keys = {
+  --   {
+  --     "<leader>cn",
+  --     function()
+  --       require("neogen").generate()
+  --     end,
+  --     desc = "Generate Annotations (Neogen)",
+  --   },
+  -- },
   opts = {
     languages = {
       python = {
@@ -14,3 +23,46 @@ return {
   -- Uncomment next line if you want to follow only stable versions
   -- version = "*"
 }
+-- return {
+--   "danymat/neogen",
+--   cmd = "Neogen",
+--   dependencies = "nvim-treesitter/nvim-treesitter",
+--   keys = {
+--     {
+--       "<leader>cn",
+--       function()
+--         require("neogen").generate()
+--       end,
+--       desc = "Generate Annotations (Neogen)",
+--     },
+--   },
+--   opts = function(_, opts)
+--     if opts.snippet_engine ~= nil then
+--       return
+--     end
+--
+--     local map = {
+--       ["LuaSnip"] = "luasnip",
+--       ["nvim-snippy"] = "snippy",
+--       ["vim-vsnip"] = "vsnip",
+--     }
+--
+--     for plugin, engine in pairs(map) do
+--       if LazyVim.has(plugin) then
+--         opts.snippet_engine = engine
+--         return
+--       end
+--     end
+--
+--     if vim.snippet then
+--       opts.snippet_engine = "nvim"
+--     end
+--   end,
+--   languages = {
+--     python = {
+--       template = {
+--         annotation_convention = "numpydoc",
+--       },
+--     },
+--   },
+-- }
