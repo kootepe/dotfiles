1563fd7ba56177ea0c2a5c1be83ed7418dc6da5e
Eero Koskinen <eekoskin22@ict-pf4z0315.oulu.fi>
2024-10-04 15:29:39 +0300

    Add option to launch zathura or any other program

--- fdo.sh
+++ fdo.sh
@@ -1,5 +1,27 @@
 #!/bin/bash
 
+# Default program to use is 'nvim'
+program="nvim"
+
+# Parse command line options
+while getopts "p:z" opt; do
+  case $opt in
+  p)
+    program="$OPTARG"
+    ;;
+  z)
+    program="zathura"
+    ;;
+  *)
+    echo "Usage: $0 [-p program] [-z] [search-pattern]"
+    exit 1
+    ;;
+  esac
+done
+
+# Shift past the processed options
+shift $((OPTIND - 1))
+
 # Check if `fd` is installed
 if ! command -v fd &>/dev/null; then
   echo "'fd' is not installed. Please install it and try again."
@@ -7,7 +29,7 @@ if ! command -v fd &>/dev/null; then
 fi
 
 # Run `fd` to search for files based on user input (pass additional fd options if needed)
-files=$(fd --max-depth 2 "$@")
+files=$(fd --max-depth 2 -E "*.tdf" "$@")
 
 # Check if any files were found
 if [ -z "$files" ]; then
@@ -21,7 +43,7 @@ keys=({1..9} {a..z})
 # Display the files with a key appended in front
 declare -A file_map
 key_index=0
-echo "Choose a file to open in vim (press the corresponding key):"
+echo "Choose a file to open with $program (press the corresponding key):"
 while IFS= read -r file; do
   key="${keys[$key_index]}"
   echo "[$key] $file"
@@ -36,9 +58,9 @@ echo -n "Press the key corresponding to the file you want to open: "
 read -n 1 choice
 echo
 
-# Open the selected file in vim
+# Open the selected file with the chosen program
 if [ -n "${file_map[$choice]}" ]; then
-  nvim "${file_map[$choice]}"
+  $program "${file_map[$choice]}"
 else
   echo "Invalid choice."
 fi
