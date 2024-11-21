4b87d46d8822c72659052f7686e7acdf63ee0636
Eero Koskinen <eekoskin22@ict-pf4z0315.oulu.fi>
2024-11-21 09:03:57 +0200

    lazyvim 13

--- nvim/lua/plugins/disabled.lua
+++ nvim/lua/plugins/disabled.lua
@@ -5,6 +5,11 @@ return {
   { "lukas-reineke/indent-blankline.nvim", enabled = true },
   { "m-demare/hlargs.nvim", enabled = true },
   { "Rrethy/vim-hexokinase", enabled = false },
+  { "Rrethy/vim-hexokinase", enabled = false },
+  { "nvimdev/dashboard-nvim", enabled = false },
+  -- { "hrsh7th/cmp-buffer", enabled = false },
+  -- { "hrsh7th/cmp-path", enabled = false },
+  -- { "petertriho/cmp-git", enabled = false },
 }
 -- this might fix nvim-cmp crashing from time to time
 -- require("nvim-autopairs").setup({check_ts = true, map_cr = false})
