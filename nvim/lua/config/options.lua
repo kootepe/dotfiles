c70d706579c3b4630a24e774e4467c47ea1f4e40
Eero Koskinen <Eero Koskinen>
2025-01-02 11:28:23 +0200

    add home variable

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,12 +1,14 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+local home_path = vim.fn.expand("$HOME")
 local o = vim.o
 local g = vim.g
 --
-g.python3_host_prog = "/home/eerokos/miniconda3/bin/python3"
+g.python3_host_prog = home_path .. "/miniconda3/bin/python3"
 o.encoding = "utf-8"
 o.cursorline = true
+o.foldmethod = "expr"
 o.cursorcolumn = true
 --
 g.loaded_netrw = 1
@@ -29,10 +31,11 @@ o.autoindent = true
 o.showtabline = 2
 o.mouse = "n"
 o.undofile = true
-o.undodir = os.getenv("HOME") .. "/.nvim/.undodir//"
+o.undodir = home_path .. "/.nvim/.undodir//"
 o.winbar = "%=%m %f"
 
-g.vimtex_view_method = "zathura"
+-- g.vimtex_view_method = "zathura"
+g.vimtex_view_method = "zathura_simple"
 g.vimtex_view_general_viewer = "zathura"
 g.vimtex_view_general_options = "--unique file:@pdf\\#src:@line@tex"
 g.vimtex_compiler_method = "latexmk"
@@ -41,3 +44,4 @@ o.ignorecase = true
 vim.filetype.add({ pattern = { [".*.ipynb"] = "ipynb" } })
 vim.filetype.add({ pattern = { [".*.CR1X"] = "vb" } })
 vim.filetype.add({ pattern = { [".*.cr1x"] = "vb" } })
+vim.filetype.add({ pattern = { ["[Dd]ockerfile.*"] = "dockerfile" } })
